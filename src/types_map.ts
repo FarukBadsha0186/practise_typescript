d03fc437ea87e35807d9dffd8d4c39f82c777c0b
FarukBadsha0186 <farukbadsha02@gmail.com>
2026-05-05 15:46:14 +0600

    Added types_map.ts file

---
+++ src/types_map.ts
@@ -0,0 +1,7 @@
+const arrayOfNum: number[] = [2, 3, 4];
+
+const arrayOfstring: string[] = ["2", "4", "5"];
+
+const arrayOfStringUsingMAp: string[] = arrayOfNum.map((num) => num.toString());
+
+console.log(arrayOfStringUsingMAp);
